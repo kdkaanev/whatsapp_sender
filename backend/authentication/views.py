4a4498aeef0ddd98b06bd6e0c5a7b33d588f8db8
kdkaanev <kdkaane@github.io>
2026-08-30 09:59:39 +0300

    up auth

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -2,6 +2,7 @@ from rest_framework import status, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+from app.tasks import send_activation_email
 from .serializers import (
     RegisterSerializer,
     UserSerializer,
